e293a365387b1a453ca8e027cfdf7b1b3b732521
Greta Workman <84729203+greetah@users.noreply.github.com>
2023-11-14 15:49:41 -0500

    Update MyV0Component.tsx

--- app/components/MyV0Component.tsx
+++ app/components/MyV0Component.tsx
@@ -1,27 +1,29 @@
-import { getNowPlaying, processPlaying } from "../lib/spotify";
+import Link from "next/link"
 
-export default async function NowPlaying() {
-  const response = await getNowPlaying();
-  let data;
-
-  if (response.status === 204 || response.status > 400) {
-    data = { isPlaying: false };
-    if (response.status === 401) {
-      console.error("TOKEN IS FUCKED: " + response.statusText);
-    }
-  } else {
-    const song = await response.json();
-    data = processPlaying(song);
-  }
-  return data?.isPlaying ? (
-    <div className="grid-rows-auto grid-flow-col gap-4 py-6 pr-4 flex-grow-1">
-      <div className="my-underline font:bold row-span-2">
-        <a href={data.songUrl}>Now Playing</a>
-      </div>
-      <div className="font-bold">
-        <p>{data.title}</p>
-        <p className="font-semibold">{data.artist}</p>
-      </div>
+export default function Component() {
+  return (
+    <div className="w-64 h-64 relative overflow-hidden bg-zinc-900 rounded-lg shadow hover:shadow-lg transition-shadow cursor-pointer">
+      <Link aria-label="Open in Spotify player" href="#">
+        <div className="absolute inset-0 flex items-center justify-center">
+          <img
+            alt="Album Cover"
+            className="object-cover w-full h-full"
+            height="256"
+            src="/placeholder.svg"
+            style={{
+              aspectRatio: "256/256",
+              objectFit: "cover",
+            }}
+            width="256"
+          />
+        </div>
+        <div className="absolute inset-0 bg-zinc-900 bg-opacity-50 flex items-end p-4">
+          <div className="text-zinc-200">
+            <h3 className="text-lg font-semibold">Song Title</h3>
+            <p className="mt-1">Artist Name</p>
+          </div>
+        </div>
+      </Link>
     </div>
-  ) : null;
+  )
 }
