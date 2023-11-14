71bf1ea2236eb683fafb9dd374085f5c0d4042ce
Greta Workman <84729203+greetah@users.noreply.github.com>
2023-11-14 15:41:57 -0500

    Update MyV0Component.tsx

--- app/components/MyV0Component.tsx
+++ app/components/MyV0Component.tsx
@@ -1,62 +1,27 @@
-// you can overwrite this entire file with your v0 Component.
-// just copy and paste the "React" output over the entire file.
+import { getNowPlaying, processPlaying } from "../lib/spotify";
 
-import Link from "next/link";
-import React from "react";
-import {V0Logo} from "./symbols";
+export default async function NowPlaying() {
+  const response = await getNowPlaying();
+  let data;
 
-function TestComponent() {
-  return (
-    <div
-      style={{
-        display: "flex",
-        height: "100%",
-        justifyContent: "space-between",
-        flexDirection: "column",
-        width: "100%",
-      }}
-    >
-      <h3
-        style={{
-          maxWidth: "11em",
-        }}
-      >
-        This would be a pretty good place for a{" "}
-        <Link href="https://v0.dev/" target="_blank" rel="noopener noreferrer">
-          v0 component
-        </Link>
-        , wouldn't it?
-      </h3>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "flex-end",
-          width: "100%",
-        }}
-      >
-        <p
-          style={{
-            maxWidth: "20em",
-          }}
-        >
-          Go make one, then paste it into{" "}
-          <code
-            style={{
-              fontFamily: "var(--font-geist-mono)",
-              fontWeight: 500,
-              fontSize: "0.95em",
-              fontFeatureSettings: "'ss09'",
-            }}
-          >
-            app/components/MyV0Component.tsx
-          </code>
-        </p>
-
-        <V0Logo />
+  if (response.status === 204 || response.status > 400) {
+    data = { isPlaying: false };
+    if (response.status === 401) {
+      console.error("TOKEN IS FUCKED: " + response.statusText);
+    }
+  } else {
+    const song = await response.json();
+    data = processPlaying(song);
+  }
+  return data?.isPlaying ? (
+    <div className="grid-rows-auto grid-flow-col gap-4 py-6 pr-4 flex-grow-1">
+      <div className="my-underline font:bold row-span-2">
+        <a href={data.songUrl}>Now Playing</a>
+      </div>
+      <div className="font-bold">
+        <p>{data.title}</p>
+        <p className="font-semibold">{data.artist}</p>
       </div>
     </div>
-  );
+  ) : null;
 }
-
-export default TestComponent;
